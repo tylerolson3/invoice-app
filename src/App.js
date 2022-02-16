f5e527f0b8094140bfc33718390888a276d81e15
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-02-15 19:08:45 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+import DetailsPage from "./routes/DetailsPage";
+import TestPage from "./routes/TestPage";
+import { CssBaseline, Box, useMediaQuery } from "@mui/material";
+import { InvoicesContextProvider } from "./context/InvoicesContext";
+import NavBarHorizontal from "./components/NavBarHorizontal";
+import NavBarVertical from "./components/NavBarVertical";
+import "./App.css";
+import styles from "./styles/styles";
 
 function App() {
+  const isMobileDisplay = useMediaQuery("(max-width:1025px)");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InvoicesContextProvider>
+      <Box sx={{ ...styles[1] }}>
+        {isMobileDisplay ? <NavBarHorizontal /> : <NavBarVertical />}
+        <Box sx={{ ...styles[2] }}>
+          <CssBaseline />
+          <Box sx={{ ...styles[3] }}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/invoices/:id" element={<DetailsPage />} />
+                <Route path="/test" element={<TestPage />} />
+              </Routes>
+            </Router>
+          </Box>
+        </Box>
+      </Box>
+    </InvoicesContextProvider>
   );
 }
 
